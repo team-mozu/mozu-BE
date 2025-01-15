89901626e9c34837abf6029dc21a0fb25250c0ce
Tyler <tylereunholee010203@gmail.com>
2025-01-15 16:57:23 +0900

    feat :: [#8] 기사 - Application - Writer Service
    이미지 업로드 기능 추가

--- src/article/application/article.write.service.ts
+++ src/article/application/article.write.service.ts
@@ -1,7 +1,11 @@
 import { ArticleDTO } from 'src/common/data/article/article.dto';
 
 export interface ArticleWriteService {
-    create(articleDTO: ArticleDTO): Promise<ArticleDTO>;
-    update(articleDTO: ArticleDTO, articleId: number): Promise<ArticleDTO>;
+    create(articleDTO: ArticleDTO, file: Express.Multer.File): Promise<ArticleDTO>;
+    update(
+        articleId: number,
+        articleDTO: ArticleDTO,
+        file: Express.Multer.File
+    ): Promise<ArticleDTO>;
     delete(articleId: number): Promise<void>;
 }
