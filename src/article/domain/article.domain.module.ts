9fb75278dcc6d571257f2b9709b6987ecf5e260e
Tyler <tylereunholee010203@gmail.com>
2025-01-15 16:55:30 +0900

    feat :: [#8] 기사 - domain - module
    Config, S3, Checker 관련 의존성 추가

--- src/article/domain/article.domain.module.ts
+++ src/article/domain/article.domain.module.ts
@@ -3,13 +3,22 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ArticleRepository } from './persistence/article.repository';
 import { ArticleEntity } from './persistence/article.entity';
 import { ArticleDomainMapper } from './persistence/article.domain.mapper';
+import { ArticleChecker } from './persistence/article.checker';
+import { S3Adapter } from 'src/common/thirdparty/s3.adapter';
+import { ConfigModule } from '@nestjs/config';
 
 const ARTICLE_REPOSITORY = { provide: 'repository', useClass: ArticleRepository };
 const ARTICLE_ENTITY = TypeOrmModule.forFeature([ArticleEntity]);
 
 @Module({
-    imports: [ARTICLE_ENTITY],
-    providers: [ARTICLE_REPOSITORY, ArticleDomainMapper, ArticleRepository],
+    imports: [ARTICLE_ENTITY, ConfigModule],
+    providers: [
+        ARTICLE_REPOSITORY,
+        ArticleDomainMapper,
+        ArticleRepository,
+        ArticleChecker,
+        S3Adapter
+    ],
     exports: [ARTICLE_REPOSITORY, ArticleDomainMapper, ArticleRepository]
 })
 export class ArticleDomainModule {}
