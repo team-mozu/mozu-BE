1df543ea7aebf50eae326b902c948206b539989c
Tyler <tylereunholee010203@gmail.com>
2025-01-15 16:54:39 +0900

    feat :: [#8] 기사 - Domain - Wrtier
    이미지 업로드 기능 추가

--- src/article/domain/article.domain.writer.ts
+++ src/article/domain/article.domain.writer.ts
@@ -1,7 +1,11 @@
 import { ArticleDTO } from 'src/common/data/article/article.dto';
 
 export interface ArticleDomainWriter {
-    save(atricleDTO: ArticleDTO): Promise<ArticleDTO>;
-    update(articleDTO: ArticleDTO): Promise<ArticleDTO>;
+    save(atricleDTO: ArticleDTO, file: Express.Multer.File): Promise<ArticleDTO>;
+    update(
+        articleId: number,
+        articleDTO: ArticleDTO,
+        file: Express.Multer.File
+    ): Promise<ArticleDTO>;
     delete(articleId: number): Promise<void>;
 }
