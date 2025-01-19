2af578ae1e376e5e8786eb1f548a57f2fbb5fde2
Tyler <tylereunholee010203@gmail.com>
2025-01-19 13:36:37 +0900

    feat :: [#10] requestForm swagger 세팅 추가

--- src/article/presentation/form/request/request.article.form.ts
+++ src/article/presentation/form/request/request.article.form.ts
@@ -1,4 +1,16 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class RequestArticleForm {
+    @ApiProperty({ description: '기사 제목' })
     title: string;
+
+    @ApiProperty({ description: '기사 설명' })
     description: string;
+
+    @ApiProperty({
+        type: 'string',
+        format: 'binary',
+        description: '첨부 파일 (이미지 업로드)'
+    })
+    file: any;
 }
