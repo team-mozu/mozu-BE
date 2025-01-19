d8a74af6e3f542a783252906bcdc70273e9ac810
Tyler <tylereunholee010203@gmail.com>
2025-01-19 13:37:39 +0900

    feat :: [#10] presentation - Adapter
    스웨거 설정 추가

--- src/article/presentation/article.read.adapter.ts
+++ src/article/presentation/article.read.adapter.ts
@@ -1,26 +1,29 @@
 import { Controller, Get, Inject, Injectable, Param } from '@nestjs/common';
-import { ArticleReadDocumentation } from './document/article.read.documentation';
 import { ArticleDTO } from 'src/common/data/article/article.dto';
 import { ResponseArticleForm } from './form/response/response.article.form';
 import { ArticleReadService } from '../application/article.read.service';
+import { ApiTags } from '@nestjs/swagger';
+import { ArticleDetailSwagger, ArticleListSwagger } from './document/article.swagger.documentation';
 
+@ApiTags('기사 API')
 @Controller('/article')
-@Injectable()
-export class ArticleReadAdapter implements ArticleReadDocumentation {
+export class ArticleReadAdapter {
     constructor(
         @Inject('read_impl')
         private readonly readService: ArticleReadService
     ) {}
 
-    @Get('/:id')
-    async getByArticleID(@Param('id') articleId: string): Promise<ArticleDTO> {
-        return await this.readService.getByArticleID(+articleId);
-    }
-
     @Get()
+    @ArticleListSwagger()
     async getArticleList(): Promise<ResponseArticleForm> {
         const articleList = await this.readService.getArticleList();
 
         return new ResponseArticleForm(articleList);
+    }
+
+    @Get('/:id')
+    @ArticleDetailSwagger()
+    async getByArticleID(@Param('id') articleId: string): Promise<ArticleDTO> {
+        return await this.readService.getByArticleID(+articleId);
     }
 }
--- src/article/presentation/article.write.adapter.ts
+++ src/article/presentation/article.write.adapter.ts
@@ -7,39 +7,47 @@ import {
     Injectable,
     Param,
     Post,
+    Put,
     UploadedFile,
     UseInterceptors
 } from '@nestjs/common';
 import { ArticleDTO } from 'src/common/data/article/article.dto';
-import { ArticleWriteDocumentation } from './document/article.write.documentation';
 import { RequestArticleForm } from './form/request/request.article.form';
 import { RequestArticleFormMapper } from './form/request/request.article.form.mapper';
 import { ArticleWriteService } from '../application/article.write.service';
-import { MozuLogger } from 'src/common/logger/mozu.logger';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { ApiProperty, ApiTags } from '@nestjs/swagger';
+import {
+    ArticleCreateSwagger,
+    ArticleDeleteSwagger,
+    ArticleUpdateSwagger
+} from './document/article.swagger.documentation';
 
+@ApiTags('기사 API')
 @Controller('/article')
 @Injectable()
-export class ArticleWriteAdapter implements ArticleWriteDocumentation {
+export class ArticleWriteAdapter {
     constructor(
         @Inject('write_impl')
         private readonly writeService: ArticleWriteService,
         private readonly requestArticleFormMapper: RequestArticleFormMapper
     ) {}
 
-    @Post('/create')
-    @UseInterceptors(FileInterceptor('image'))
+    @Post()
+    @UseInterceptors(FileInterceptor('file'))
+    @ArticleCreateSwagger()
     async create(
-        @Body() form: RequestArticleForm,
-        @UploadedFile() file: Express.Multer.File
-    ): Promise<ArticleDTO> {
-        const internalDTO = await this.requestArticleFormMapper.toDTO(form);
+        @UploadedFile() file: Express.Multer.File,
+        @Body() formData: RequestArticleForm
+    ): Promise<any> {
+        const internalDTO = await this.requestArticleFormMapper.toDTO(formData);
 
         return await this.writeService.create(internalDTO, file);
     }
 
-    @Post('/update/:id')
+    @Put('/:id')
     @UseInterceptors(FileInterceptor('image'))
+    @ArticleUpdateSwagger()
     async update(
         @Param('id') articleId: string,
         @Body() form: RequestArticleForm,
@@ -50,7 +58,8 @@ export class ArticleWriteAdapter implements ArticleWriteDocumentation {
         return await this.writeService.update(+articleId, internalDTO, file);
     }
 
-    @Delete('/delete/:id')
+    @Delete('/:id')
+    @ArticleDeleteSwagger()
     async delete(@Param('id') articleId: string): Promise<void> {
         return await this.writeService.delete(+articleId);
     }
