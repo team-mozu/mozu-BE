c6dcfb8be19b9b3948180148958048e9a764c25c
Tyler <tylereunholee010203@gmail.com>
2025-02-10 19:49:20 +0900

    refactor :: [#19] article - application - read service

--- src/article/application/article.read.service.ts
+++ src/article/application/article.read.service.ts
@@ -2,5 +2,5 @@ import { ArticleDTO } from 'src/common/data/article/article.dto';
 
 export interface ArticleReadService {
     getByArticleID(articleId: number): Promise<ArticleDTO>;
-    getArticleList(): Promise<ArticleDTO[]>;
+    getArticleList(organId: number): Promise<ArticleDTO[]>;
 }
