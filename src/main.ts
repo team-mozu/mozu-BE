d1ca6de577d3c31632a387ea2a542dd949440d97
Tyler <tylereunholee010203@gmail.com>
2025-02-10 19:46:37 +0900

    feat :: [#19] exception filter 적용

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/filter/global.exception.filter';
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.useGlobalFilters(new HttpExceptionFilter(new Logger()));
+    app.useGlobalPipes(
+        new ValidationPipe({
+            whitelist: true,
+            forbidNonWhitelisted: true,
+            transform: true
+        })
+    );
+
     await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
