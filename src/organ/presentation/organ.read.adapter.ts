eff519dceda9e206c36c0fd53c81bf50832f6a2b
Tyler <tylereunholee010203@gmail.com>
2025-02-12 10:52:04 +0900

    feat :: [#21] token reissue 기능 개발
    refreshToken을 이용한 accessToken 재발급 기능 개발 API 명세서 참고

--- src/organ/presentation/organ.read.adapter.ts
+++ src/organ/presentation/organ.read.adapter.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Inject, Param, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Inject, Param, Post, UseGuards, Headers } from '@nestjs/common';
 import { OrganReadService } from '../application/organ.read.service';
 import { OrganDTO } from '../common/data/organ.dto';
 import { RequestLoginForm } from './form/request/request.login.form';
@@ -7,6 +7,7 @@ import { RequestOrganFormMapper } from './form/request/request.organ.form.mapper
 import { JwtAuthGuard } from 'src/common/guard/jwt.guard';
 import { Permission } from 'src/common/decorator/authority.decorator';
 import { Authority } from 'src/common/data/Role';
+import { ResponseReissueForm } from './form/response/response.reissue.form';
 
 @Controller('/organ')
 export class OrganReadAdapter {
@@ -28,6 +29,13 @@ export class OrganReadAdapter {
     @Permission([Authority.ORGAN])
     async getOrganList(): Promise<OrganDTO[]> {
         return await this.readService.getOrganList();
+    }
+
+    @Get('/token/re-issue')
+    async tokenReissue(@Headers('rf-token') refreshToken: string): Promise<ResponseReissueForm> {
+        const newToken = await this.readService.tokenReissue(refreshToken);
+
+        return new ResponseReissueForm(newToken);
     }
 
     @Post('/login')
