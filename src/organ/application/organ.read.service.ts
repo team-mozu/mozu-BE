9fa27440231612c4cefa3f1dd1c61eb479ffc8de
Tyler <tylereunholee010203@gmail.com>
2025-02-12 10:53:18 +0900

    feat :: [#21] token reissue

--- src/organ/application/organ.read.service.ts
+++ src/organ/application/organ.read.service.ts
@@ -4,4 +4,5 @@ export interface OrganReadService {
     getByOrganID(organId: number): Promise<OrganDTO>;
     getOrganList(): Promise<OrganDTO[]>;
     login(input: OrganDTO): Promise<{ accessToken: string; refreshToken: string }>;
+    tokenReissue(refreshToken: string): Promise<string>;
 }
