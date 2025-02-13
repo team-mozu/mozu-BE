a6eaa1dc2ea7a748baebb5b7d0cf77c5258e4c8e
박지민 <pjmlove2008@gmail.com>
2025-02-14 08:14:05 +0900

    refactor :: [#13] hold item - presentation - read documentation
    read documentation 파일 삭제

--- src/hold-item/presentation/document/hold-item.read.documentation.ts
+++
@@ -1,7 +0,0 @@
-import { HoldItemDTO } from "src/common/data/hold-item/hold-item.dto";
-import { ResponseHoldItemForm } from "../form/response/response.hold-item.form";
-
-export interface HoldItemReadDocumentation {
-    getByHoldItemID(HoldItemId: string): Promise<HoldItemDTO>;
-    getHoldItemList: (classTeamId: string) => Promise<HoldItemDTO[]>
-}
