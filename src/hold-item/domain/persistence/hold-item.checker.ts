b491c8b1ac7480df6d8abfda0da3372560e5d933
박지민 <pjmlove2008@gmail.com>
2025-02-14 08:39:38 +0900

    refactor :: [#13] hold item - domain - persistence - checker
    내장 예외 필터 변경

--- src/hold-item/domain/persistence/hold-item.checker.ts
+++ src/hold-item/domain/persistence/hold-item.checker.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@nestjs/common";
+import { BadRequestException, Injectable, NotFoundException } from "@nestjs/common";
 import { HoldItemEntity } from "./hold-item.entity";
 
 @Injectable()
@@ -6,17 +6,17 @@ export class HoldItemChecker {
 
     existValidate(holdItem: HoldItemEntity, holdItemId: string): void {
         if (!holdItem) {
-            throw new Error(`해당하는 보유주식(${holdItemId})이 존재하지 않습니다.`);
+            throw new NotFoundException(`해당하는 보유주식(${holdItemId})이 존재하지 않습니다.`);
         }
     }
 
     formValidate(holdItemEntity: HoldItemEntity): void {
         if (!holdItemEntity.itemName || !holdItemEntity.itemCnt) {
-            throw new Error("아이템이름과 갯수는 필수 항목입니다.");
+            throw new BadRequestException("아이템 이름과 갯수는 필수 항목입니다.");
         }
 
         if (holdItemEntity.buyMoney <= 0 || holdItemEntity.valProfit < 0) {
-            throw new Error("잘못된 구매 금액 또는 이익 값입니다.");
+            throw new BadRequestException("잘못된 구매 금액 또는 이익 값입니다.");
         }
     }
 }
