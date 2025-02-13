094d93b573adcc75316fc953d2576bccdfab3ac8
박지민 <pjmlove2008@gmail.com>
2025-02-14 08:25:13 +0900

    refactor :: [#13] hold item - domain - mapper
    코드 리팩토링

--- src/hold-item/domain/persistence/hold-item.domain.mapper.ts
+++ src/hold-item/domain/persistence/hold-item.domain.mapper.ts
@@ -1,16 +1,15 @@
 import { Injectable } from "@nestjs/common";
 import { HoldItemEntity } from "./hold-item.entity";
-import { promises } from "dns";
 import { HoldItemDTO } from "src/common/data/hold-item/hold-item.dto";
 
 @Injectable()
 export class HoldItemDomainMapper {
     constructor() {}
 
-    async toDomain(holdItemEntity: HoldItemEntity, classTeamId: string): Promise<HoldItemDTO> {
+    async toDomain(holdItemEntity: HoldItemEntity): Promise<HoldItemDTO> {
         if (!holdItemEntity) return null;
         
-                const {id, itemName, itemCnt, buyMoney, valProfit, profitNum} = holdItemEntity;
+                const {id,classTeamId, itemName, itemCnt, buyMoney, valProfit, profitNum} = holdItemEntity;
         
                 return new HoldItemDTO(id, classTeamId, itemName, itemCnt, buyMoney, valProfit, profitNum);
     }
