9d9ac210eb4228f0939b80d6334a5ac24ac03fb0
박지민 <pjmlove2008@gmail.com>
2025-02-14 08:19:16 +0900

    refactor :: [#13] hold item - presentation - write adapter
    불필요한 데코레이터 삭제 및 의존성 키 값 통일, Documentation 삭제

--- src/hold-item/presentation/hold-item.write.adapter.ts
+++ src/hold-item/presentation/hold-item.write.adapter.ts
@@ -1,27 +1,19 @@
 import { Body, Controller, Inject, Injectable, Post } from "@nestjs/common";
-import { HoldItemeWriteDocumentation } from "./document/hold-item.write.documentation";
 import { HoldItemWriteService } from "../application/hold-item.write.service";
 import { RequestHoldItemForm } from "./form/request/request.hold-item.form";
 import { HoldItemDTO } from "src/common/data/hold-item/hold-item.dto";
+import { RequestHoldItemFormMapper } from "./form/request/request.hold-item.from.mapper";
 
 @Controller("/hold-item")
-@Injectable()
-export class HoldItemWriteAdapter implements HoldItemeWriteDocumentation{
+export class HoldItemWriteAdapter{
     constructor(
-        @Inject('hold_item_write_impl')
-                private readonly holdItemWriteService: HoldItemWriteService
+        @Inject('write_impl')
+                private readonly holdItemWriteService: HoldItemWriteService,
+                private readonly mapper: RequestHoldItemFormMapper
     ){}
 
     @Post("/create")
     async create(@Body() requestForm: RequestHoldItemForm): Promise<HoldItemDTO> {
-        return await this.holdItemWriteService.save(new HoldItemDTO(
-            requestForm.id,
-            requestForm.classTeamId,
-            requestForm.itemName,
-            requestForm.itemCnt,
-            requestForm.buyMoney,
-            requestForm.valProfit,
-            requestForm.profitNum
-        ));
+        return await this.holdItemWriteService.save(this.mapper.toDTO(requestForm));
     }
 }
