ac48add2e428b2bb613be5f2c8a5ce24738eb1da
박지민 <pjmlove2008@gmail.com>
2025-02-14 08:15:53 +0900

    refactor :: [#13] hold item - presentation - write documentation
    write documentation 파일 삭제

--- src/hold-item/presentation/document/hold-item.write.documentation.ts
+++
@@ -1,6 +0,0 @@
-import { HoldItemDTO } from "src/common/data/hold-item/hold-item.dto";
-import { RequestHoldItemForm } from "../form/request/request.hold-item.form";
-
-export interface HoldItemeWriteDocumentation {
-    create(createArticleDTO: RequestHoldItemForm): Promise<HoldItemDTO>;
-}
