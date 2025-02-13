89556148893d4cd86676accc9e0a118f4d1b5daf
박지민 <pjmlove2008@gmail.com>
2025-02-14 08:21:30 +0900

    refactor :: [#13] hold item - presentation - read adapter
    불필요한 데코레이터 삭제 및 의존성 키 값 통일, Documentation 삭제, 데코레이터 수정

--- src/hold-item/presentation/hold-item.read.adapter.ts
+++ src/hold-item/presentation/hold-item.read.adapter.ts
@@ -1,13 +1,11 @@
-import { Body, Controller, Get, Inject, Injectable, Param } from "@nestjs/common";
-import { HoldItemReadDocumentation } from "./document/hold-item.read.documentation";
+import { Body, Controller, Get, Inject, Injectable, Param, Query } from "@nestjs/common";
 import { HoldItemReadService } from "../application/hold-item.read.service";
 import { HoldItemDTO } from "src/common/data/hold-item/hold-item.dto";
 
 @Controller("/hold-item")
-@Injectable()
-export class HoldItemReadAdapter implements HoldItemReadDocumentation{
+export class HoldItemReadAdapter{
     constructor(
-        @Inject('hold_item_read_impl')
+        @Inject('read_impl')
         private readonly holdItemReadService: HoldItemReadService
     ){}
 
@@ -16,8 +14,8 @@ export class HoldItemReadAdapter implements HoldItemReadDocumentation{
         return await this.holdItemReadService.findByHoldItemId(holdItemId);
     }
 
-    @Get()
-    async getHoldItemList(@Body("classTeamId") classTeamId: string): Promise<HoldItemDTO[]> {
+    @Get(":classTeamId")
+    async getHoldItemList(@Param("classTeamId") classTeamId: string): Promise<HoldItemDTO[]> {
         return await this.holdItemReadService.findByHoldItemList(classTeamId);
     }
 }
