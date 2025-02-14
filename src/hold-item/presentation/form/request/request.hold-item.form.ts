b07ee82893c3a8dc5cf33deead78b9fcdb385192
박지민 <pjmlove2008@gmail.com>
2025-02-14 16:18:41 +0900

    style :: [#13] hold item - presentation - request form
    줄 간격 정리

--- src/hold-item/presentation/form/request/request.hold-item.form.ts
+++ src/hold-item/presentation/form/request/request.hold-item.form.ts
@@ -1,18 +1,26 @@
 import { IsNumber, IsString } from "class-validator";
 
 export class RequestHoldItemForm{
+    
     @IsString()
     id: string;
+
     @IsString()
     classTeamId: string;
+
     @IsString()
     itemName: string;
+
     @IsNumber()
     itemCnt: number;
+
     @IsNumber()
     buyMoney: number;
+
     @IsNumber()
     valProfit: number;
+
     @IsNumber()
     profitNum: number;
+
 }
