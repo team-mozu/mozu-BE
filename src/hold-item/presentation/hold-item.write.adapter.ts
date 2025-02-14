a745ede13e8c0105db4e78f42218f628a1c68fd3
박지민 <pjmlove2008@gmail.com>
2025-02-14 15:47:49 +0900

    refactor :: [#13] hold item - presentation - write adapter
    HoldItemDTO 변환 방식 개선

--- src/hold-item/presentation/hold-item.write.adapter.ts
+++ src/hold-item/presentation/hold-item.write.adapter.ts
@@ -2,18 +2,25 @@ import { Body, Controller, Inject, Injectable, Post } from "@nestjs/common";
 import { HoldItemWriteService } from "../application/hold-item.write.service";
 import { RequestHoldItemForm } from "./form/request/request.hold-item.form";
 import { HoldItemDTO } from "src/common/data/hold-item/hold-item.dto";
-import { RequestHoldItemFormMapper } from "./form/request/request.hold-item.from.mapper";
 
 @Controller("/hold-item")
+@Injectable()
 export class HoldItemWriteAdapter{
     constructor(
         @Inject('write_impl')
-                private readonly holdItemWriteService: HoldItemWriteService,
-                private readonly mapper: RequestHoldItemFormMapper
+                private readonly holdItemWriteService: HoldItemWriteService
     ){}
 
     @Post("/create")
     async create(@Body() requestForm: RequestHoldItemForm): Promise<HoldItemDTO> {
-        return await this.holdItemWriteService.save(this.mapper.toDTO(requestForm));
+        return await this.holdItemWriteService.save(new HoldItemDTO(
+            requestForm.id,
+            requestForm.classTeamId,
+            requestForm.itemName,
+            requestForm.itemCnt,
+            requestForm.buyMoney,
+            requestForm.valProfit,
+            requestForm.profitNum
+        ));
     }
 }
