8727ebcc7c1741665dd6e2d5fac811b03dc44203
박지민 <pjmlove2008@gmail.com>
2025-02-14 15:55:11 +0900

    feat :: [#13] hold item - presentation - write adapter
    입력 검증 추가

--- src/hold-item/presentation/hold-item.write.adapter.ts
+++ src/hold-item/presentation/hold-item.write.adapter.ts
@@ -1,7 +1,7 @@
-import { Body, Controller, Inject, Injectable, Post } from "@nestjs/common";
-import { HoldItemWriteService } from "../application/hold-item.write.service";
+import { Body, Controller, Inject, Injectable, Post, UsePipes, ValidationPipe } from "@nestjs/common";
 import { RequestHoldItemForm } from "./form/request/request.hold-item.form";
 import { HoldItemDTO } from "src/common/data/hold-item/hold-item.dto";
+import { HoldItemWriteService } from "../application/hold-item.write.service";
 
 @Controller("/hold-item")
 @Injectable()
@@ -12,6 +12,7 @@ export class HoldItemWriteAdapter{
     ){}
 
     @Post("/create")
+    @UsePipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }))
     async create(@Body() requestForm: RequestHoldItemForm): Promise<HoldItemDTO> {
         return await this.holdItemWriteService.save(new HoldItemDTO(
             requestForm.id,
