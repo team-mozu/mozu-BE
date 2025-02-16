ff5e51ff8121ba0c5b5d89a5206649ca6f97d0f2
Tyler <tylereunholee010203@gmail.com>
2025-02-16 11:40:43 +0900

    feat :: [#12] app module
    전역 모듈 의존성 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,10 +7,14 @@ import { MozuLoggerModule } from './common/logger/mozu.logger.module';
 import { ClassModule } from './class/class.module';
 import { OrganModule } from './organ/organ.module';
 import { ItemModule } from './item/item.module';
+import { JwtModule } from '@nestjs/jwt';
+import { GlobalJwtModule } from './common/module/jwt.module';
+import { GlobalConfigModule } from './common/module/config.module';
 
 @Module({
     imports: [
-        ConfigModule.forRoot(),
+        GlobalConfigModule,
+        GlobalJwtModule,
         TypeOrmModule.forRootAsync({
             imports: [ConfigModule],
             useClass: TypeOrmConfigService
