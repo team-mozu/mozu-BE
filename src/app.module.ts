73c503a33dfa6c3c10ee7cdf3013a03569619f72
Tyler <tylereunholee010203@gmail.com>
2025-02-25 20:32:47 +0900

    feat :: [#29] appmodule
    team 참여 팀 도메인 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,6 +11,7 @@ import { ItemModule } from './item/item.module';
 import { JwtModule } from '@nestjs/jwt';
 import { GlobalJwtModule } from './common/module/jwt.module';
 import { GlobalConfigModule } from './common/module/config.module';
+import { TeamModule } from './team/team.module';
 
 @Module({
     imports: [
@@ -26,6 +27,7 @@ import { GlobalConfigModule } from './common/module/config.module';
         ClassModule,
         OrganModule,
         ItemModule,
+        TeamModule,
         MozuLoggerModule
     ],
     controllers: [],
