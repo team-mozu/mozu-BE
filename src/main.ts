2e3ad684341386bd2866d19407f334f9fc01b5a0
Tyler <tylereunholee010203@gmail.com>
2025-03-01 15:15:53 +0900

    feat :: CORS 정책 허용

--- src/main.ts
+++ src/main.ts
@@ -14,10 +14,22 @@ async function bootstrap() {
         })
     );
     app.enableCors({
-        origin: '*', // 모든 도메인 허용 (개발용)
-        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-        allowedHeaders: '*',
-        credentials: true
+        origin: [
+            'http://localhost:3002', // 어드민
+            'http://localhost:3001' // 학생
+        ],
+        methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
+        allowedHeaders: [
+            'Content-Type',
+            'Authorization',
+            'Accept',
+            'rf-token', // 명시적으로 커스텀 헤더 추가
+            'X-Requested-With',
+            'Origin'
+        ],
+        credentials: true,
+        preflightContinue: false,
+        optionsSuccessStatus: 204
     });
 
     await app.listen(process.env.PORT ?? 3000);
