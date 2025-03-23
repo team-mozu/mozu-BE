d0ef9f31c3464fe9d12e42521017c96d8d2e9a1e
Tyler <tylereunholee010203@gmail.com>
2025-03-23 22:52:10 +0900

    set :: CORS 정책 전체 허용

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,7 @@ async function bootstrap() {
     );
 
     app.enableCors({
-        origin: ['https://mozu-admin-stag.vercel.app/'],
+        origin: true,
         methods: '*',
         allowedHeaders: '*',
         credentials: true,
